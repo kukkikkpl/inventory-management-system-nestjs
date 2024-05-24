a96bd9f7976934dbe93474550d47b0ff2daeb1e7
kukkikkpl <kukkik.kpl@gmail.com>
2024-05-24 18:08:04 +0700

    Install packages and add database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseConfigService } from './common/configs/db.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
